dfc7c091e2a230566fb3a858d8c692d40ca85172
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:41:46 +0530

    Update User_Exception.java

--- Custom_Class/User_Exception.java
+++ Custom_Class/User_Exception.java
@@ -1,9 +1,9 @@
 package Exception;
-
+// User_Exception class  
 public class User_Exception extends Exception {
 
-	User_Exception(String msg)
+	User_Exception(String msg) // construtor calling by the another class
 	{
-		super(msg);
+		super(msg); // super method passing the message to parent class
 	}
 }
