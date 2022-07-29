7701b9a4343525a5cb32be37870b3d0079cbface
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:46:35 +0530

    Update User_Define.java

--- Custom_Class/User_Define.java
+++ Custom_Class/User_Define.java
@@ -1,33 +1,33 @@
 package Exception;
-
 import java.util.Scanner;
 
 public class User_Define {
-	public static void main(String[] args)
+		
+	public void excp() // excp method checking a no is greater than zero
 	{
-		User_Define ob = new User_Define();
-		ob.excp();
-	}
-	
-	public void excp()
-	{
-		try
+		try 
 		{
 			Scanner sc = new Scanner(System.in);
-			System.out.print("Enter a no : ");
+			System.out.print("Enter a no : "); // taking a no from user
 			int num = sc.nextInt();
-			if(num >0)
+			if(num >0) // no is greater than zero
 			{
-				throw new User_Exception("The no is greater then Zero");
+				throw new User_Exception("The no is greater then Zero"); 
 			}
-			else if(num == 0 || num < 0)				
+			else if(num == 0 || num < 0)	// no is less than zero			
 			{
 				throw new User_Exception("The no is lesser then Zero");
 			}
 		}
-		catch(Exception e)
+		catch(Exception e) // if exception is throw than catch block is catched 
 		{
 			System.out.println("Message : "+e.getMessage());
 		}
 	}
+	public static void main(String[] args) // main method
+	{
+		User_Define ob = new User_Define(); 
+		ob.excp();
+	}
+	
 }
