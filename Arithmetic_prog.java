f5224c50accdc01e4ab73ee3dec37fd94479b63b
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:53:20 +0530

    Update Arithmetic_prog.java

--- Arithmetic_prog.java
+++ Arithmetic_prog.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 
 public class Arithmetic_prog {
 
-	int num1,num2,res;
-	public static void main(String[] args) 
+	int num1,num2,res; // global variable
+	public static void main(String[] args) // main method 
 	{
 		// TODO Auto-generated method stub
 		Arithmetic_prog obj=new Arithmetic_prog();
-		obj.Div_pro();
+		obj.Div_pro(); // Div_pro method is calling
 
 	}
-	public void Div_pro()
+	public void Div_pro() // div_pro method 
 	{
 		try
 		{
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter a number :");
+		System.out.print("Enter a number :"); // taking a two no  from user
 		num1=sc.nextInt();
 		num2=sc.nextInt();
-		res=num1/num2;
+		res=num1/num2; // division a no here
 		System.out.println("The div is = "+res);
-		res=num1+num2;
+		res=num1+num2; // addition a no here
 		System.out.println("The add is = "+res);
 		}
-		catch(Exception e)
+		catch(Exception e) // catch block to handle the exception
 		{
 			//System.out.println("The MSG="+e.getMessage());
 			System.out.println("The second no cannbot ne zero .   :)"+e.getMessage());
