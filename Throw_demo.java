fa3356c01745af2fb3f3a538b37b153db84666ac
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 20:20:08 +0530

    Update Throw_demo.java

--- Throw_demo.java
+++ Throw_demo.java
@@ -4,38 +4,36 @@ public class Throw_demo
 {
 	int num;
 	Scanner sc=new Scanner(System.in);
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		Throw_demo obj=new Throw_demo();
-		obj.even_odd();
-	}
 
-	public void even_odd()
+	public void even_odd() // even_odd method
 	{
-		try
+		try 
 		{
-			System.out.print("Enter a number : ");
+			System.out.print("Enter a number : "); // taking a no from user
 			num=sc.nextInt();
-			check_demo(num);
+			check_demo(num); // check_demo method is calling
 		}
-		catch (ArithmeticException e)
+		catch (ArithmeticException e) // catching an exception
 		{
-			System.out.println("MSG = "+ e.getMessage());
+			System.out.println("Message = "+ e.getMessage());
 		}
 	}
 
-	 void check_demo(int num2) 
+	 void check_demo(int num2) // this method is calling by the even method
 	 {
+		 if(num2 %2==0) // logic of even no 
+		 {
+			 throw new ArithmeticException("It is an even Number ...."); // throwing a exception 
+		 }
+		 else // odd no exception printing
+		 {
+			 throw new ArithmeticException("It is odd"); // throwing a exception 
+		 }
+	}
+	public static void main(String[] args) // main method 
+	{
 		// TODO Auto-generated method stub
-		 if(num2 %2==0)
-		 {
-			 throw new ArithmeticException("It is an even Number ....");
-		 }
-		 else
-		 {
-			 throw new ArithmeticException("It is odd");
-		 }
-		
+		Throw_demo obj=new Throw_demo(); 
+		obj.even_odd();
 	}
 }
